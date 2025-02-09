99bc4bfbf9936a16c37691e74bb580f0788836f3
Jaswanthi Regalla <jaswanthi@Jaswanthis-Laptop.local>
2025-02-09 10:16:06 -0800

    Changes Done

--- server.js
+++ server.js
@@ -6,49 +6,37 @@ app.use(cors())
 const path = require("path")
 const dbPath = path.join(__dirname, "simpleTodos.db")
 const {open} = require('sqlite')
-const sqlite3 = require('sqlite3')
-let db = null 
+const Database = require('better-sqlite3')
+const db = new Database(dbPath, { verbose: console.log });
 
 
-
-const initializeDBAndServer = async () => {
-    try {
-        db = await open({
-            filename: dbPath,
-            driver: sqlite3.Database
-        })
-        app.listen(4000, () => {
+app.listen(4000, () => {
             console.log("Server Running at http://localhost:4000/")
-        })
-    }
-   catch(e) {
-    console.log(`DB Error: ${e.message}`)
-    process.exit(1);
-   }
-}
-initializeDBAndServer()
+     })
+  
 
-app.get("/todos", async (request, response) => {
+
+app.get("/todos", (request, response) => {
     const getTodosQuery = `SELECT * FROM todos`;
-    const todosArray = await db.all(getTodosQuery)
+    const todosArray =  db.prepare(getTodosQuery).all()
     response.send(todosArray)
 })
 
-app.delete("/todos/:todoId/", async (request, response) => { 
+app.delete("/todos/:todoId/", (request, response) => { 
     const {todoId} = request.params
     const deleteTodosQuery = `DELETE FROM todos WHERE id=${todoId}`;
-    await db.run(deleteTodosQuery) 
+     db.prepare(deleteTodosQuery).run()
     response.send("Todo Deleted Successfully")
 
 })
 
-app.post("/todos", async (request, response) => {
+app.post("/todos",  (request, response) => {
     const todoDetails = request.body 
     const {title} = todoDetails
     const addTodoQuery = `INSERT INTO todos (title) VALUES (
                                 '${title}'
                                 );`;
-    await db.run(addTodoQuery)
+    db.prepare(addTodoQuery).run()
     response.send("Todo Added Successfully")
 })
 
