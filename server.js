56a862af1501c9a80e738e2e209cd7fa7f86c188
Jaswanthi Regalla <jaswanthi@Jaswanthis-Laptop.local>
2025-02-09 08:57:02 -0800

    Changes Commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+const cors = require("cors")
+app.use(cors())
 const path = require("path")
 const dbPath = path.join(__dirname, "simpleTodos.db")
 const {open} = require('sqlite')
